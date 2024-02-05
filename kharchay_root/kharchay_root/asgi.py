24871506ba0a9c65018e5088147b7e33b29dfe94
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-02-05 23:32:12 +0530

    changing kharchay_code remaining variable names

--- kharchay_root/kharchay_root/asgi.py
+++ kharchay_root/kharchay_root/asgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import sys
 
 from django.core.asgi import get_asgi_application
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kharchay_root.settings')
 
